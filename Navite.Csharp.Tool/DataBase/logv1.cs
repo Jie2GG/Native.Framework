b4d85de4baffced3b3d4479501d14f3d6751f6f7
kotone <i@kotone.me>
2018-12-14 10:39:09 +0800

    新增SQLite支持及提酷Q相应Table

---
+++ Navite.Csharp.Tool/DataBase/logv1.cs
@@ -0,0 +1,26 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    [Table("log")]
+    public class CQDBLog
+    {
+        [PrimaryKey, AutoIncrement, Column("ID")]
+        public int Id { get; set; }
+        [Column("time")]
+        public long Time { get; set; }
+        [Column("priority")]
+        public long Priority { get; set; }
+        [Column("source")]
+        public string Source { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("detail")]
+        public string Detail { get; set; }
+    }
+}
