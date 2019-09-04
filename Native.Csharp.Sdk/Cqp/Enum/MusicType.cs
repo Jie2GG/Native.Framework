050c2e3fb6101a8b697ee73f9f8e97af8ed84f82
JieGG <947295340@qq.com>
2019-09-04 23:28:58 +0800

    同步官方SDK更新

---
+++ Native.Csharp.Sdk/Cqp/Enum/MusicType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Native.Csharp.Sdk.Cqp.Enum
+{
+    /// <summary>
+    /// 音乐类型
+    /// </summary>
+    [DefaultValue (MusicType.Tencent)]
+    public enum MusicType
+    {
+        /// <summary>
+        /// QQ 音乐
+        /// </summary>
+        [Description ("qq")]
+        Tencent,
+        /// <summary>
+        /// 网易云音乐
+        /// </summary>
+        [Description ("163")]
+        Netease,
+        /// <summary>
+        /// 虾米音乐
+        /// </summary>
+        [Description ("xiami")]
+        XiaMi
+    }
+}
